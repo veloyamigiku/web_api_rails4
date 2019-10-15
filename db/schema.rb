8defe22e7db18f205513b4857e875b9ad9d4510e
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2019-10-15 18:43:02 +0900

    ・ユーザ管理のモデルを作成する。

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190928024651) do
+ActiveRecord::Schema.define(version: 20191015081728) do
 
   create_table "memos", force: :cascade do |t|
     t.string   "title"
     t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
